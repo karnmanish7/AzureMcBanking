fffa884d0482b89d564bb9f408037ad475f77a2d
karnmanish7 <m.karn2011@gmail.com>
2022-05-02 10:34:27 +0530

    added dbcontext

---
+++ src/Services/CustomerBanking/CustomerBanking.API/DbContext/BankApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using CustomerBanking.API.IdentityAuth;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerBanking.API.DbContext
+{
+    public class BankApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public BankApplicationDbContext(DbContextOptions<BankApplicationDbContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
